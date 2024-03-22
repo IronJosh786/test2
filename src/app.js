3f718033286fb9663ba0990f9b5cd87d6614b0ff
Faizan <sfaizan78692@gmail.com>
2024-03-22 16:34:52 +0530

    add cors origin

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: process.env.CORS_ORIGIN,
+        origin: "https://money-transfer-two.vercel.app",
         credentials: true,
     })
 );
